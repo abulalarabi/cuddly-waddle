7f2565a538e6035ccaf3b5c02fa191501195a0eb
Al Arabi <arabiabulal@gmail.com>
2023-03-06 12:04:40 -0600

    updated

---
+++ paperjs_template/templates/2.ShapeCircle.js
@@ -0,0 +1,24 @@
+// circle origin
+// create an origin point using new Point(x,y);
+//*** your code ***//
+
+
+
+// set the radius
+//*** your code ***//
+
+
+
+// create a circular path using new Path.Circle(origin, radius);
+//*** your code ***//
+
+
+
+// stylize: set strokeColor, fillColor and strokeWidth of the circle
+//*** your code ***//
+
+
+
+// remove the last segment using your_circle.removeSegment(index) method
+//*** your code ***//
+
